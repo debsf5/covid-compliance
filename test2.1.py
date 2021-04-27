542e0263d3d1e7fc2fe5270dd8d20fe3c2edb6d6
debsf5 <57457964+debsf5@users.noreply.github.com>
2021-04-27 09:36:46 +0800

    Update test2.1.py

--- test2.1.py
+++ test2.1.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import seaborn
 
 nodes = 10000
-connections = 6
+connections = 12
 # INIT_EXPOSED = int(nodes*0.01)
 
 beta = 0.155
@@ -46,25 +46,25 @@ print("Percentage of people infected at peak is " + str(peak))
 
 
 # Partial compliance (i) (comply with travelling restrictions)
-# model_partialcomplianceone = SEIRSNetworkModel(G=baseGraph, beta=beta, sigma=sigma, gamma=gamma, initE=20, budget='unequal', p_local=0.8, p_global=0.05)
-# model_partialcomplianceone.run(T=1000)
-#
-# model_partialcomplianceone.figure_infections(plot_S=False, plot_E=False, plot_I='line', plot_R='line', plot_Q_E=False, plot_Q_I=False, ylim=0.16, figsize=(5,4))
-# inf = 100*((nodes - model_partialcomplianceone.numS[-1])/nodes)
-# peak = 100*(max(model_partialcomplianceone.numI)/nodes)
-# print("Total percentage of infected is " + str(inf))
-# print("Percentage of people infected at peak is " + str(peak))
+model_partialcomplianceone = SEIRSNetworkModel(G=baseGraph, beta=beta, sigma=sigma, gamma=gamma, initE=20, budget='unequal', p_local=0.8, p_global=0.05)
+model_partialcomplianceone.run(T=1000)
+
+model_partialcomplianceone.figure_infections(plot_S=False, plot_E=False, plot_I='line', plot_R='line', plot_Q_E=False, plot_Q_I=False, ylim=0.16, figsize=(5,4))
+inf = 100*((nodes - model_partialcomplianceone.numS[-1])/nodes)
+peak = 100*(max(model_partialcomplianceone.numI)/nodes)
+print("Total percentage of infected is " + str(inf))
+print("Percentage of people infected at peak is " + str(peak))
 
 
 # Partial compliance (ii) (comply with close contacts)
-# model_partialcompliancetwo = SEIRSNetworkModel(G=baseGraph, beta=beta, sigma=sigma, gamma=gamma, initE=20, budget='unequal', p_local=0.6, p_global=0.25)
-# model_partialcompliancetwo.run(T=1000)
-#
-# model_partialcompliancetwo.figure_infections(plot_S=False, plot_E=False, plot_I='line', plot_R='line', plot_Q_E=False, plot_Q_I=False, ylim=0.16, figsize=(5,4))
-# inf = 100*((nodes - model_partialcompliancetwo.numS[-1])/nodes)
-# peak = 100*(max(model_partialcompliancetwo.numI)/nodes)
-# print("Total percentage of infected is " + str(inf))
-# print("Percentage of people infected at peak is " + str(peak))
+model_partialcompliancetwo = SEIRSNetworkModel(G=baseGraph, beta=beta, sigma=sigma, gamma=gamma, initE=20, budget='unequal', p_local=0.6, p_global=0.25)
+model_partialcompliancetwo.run(T=1000)
+
+model_partialcompliancetwo.figure_infections(plot_S=False, plot_E=False, plot_I='line', plot_R='line', plot_Q_E=False, plot_Q_I=False, ylim=0.16, figsize=(5,4))
+inf = 100*((nodes - model_partialcompliancetwo.numS[-1])/nodes)
+peak = 100*(max(model_partialcompliancetwo.numI)/nodes)
+print("Total percentage of infected is " + str(inf))
+print("Percentage of people infected at peak is " + str(peak))
 
 
 # 0% compliance
